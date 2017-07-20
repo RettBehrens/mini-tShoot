14023a37362bbf4b4a716c01b4ca18777897ce0b
RettBehrens <everettbehrens@gmail.com>
2017-07-20 09:41:00 -0600

    fixes Steve's fuckups

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 function addProjects() {
-	var projectDiv = document.getElementById("#ourProjects");
+	var projectDiv = document.getElementById("ourProjects");
 	for (var i =0; i < 4; i++) {
 		var project = document.createElement("div");
 		var projectLink = document.createElement("a");
 		var projectImage = document.createElement("img");
-		projectLink.setAttribute("href", "http://www.zombo.com");
-		projectImage.setAttribute("src", "project"+1+".png");
+		project.setAttribute("class", "project");
+		projectLink.setAttribute("href", "https://github.com/RettBehrens/mini-tShoot");
+		projectImage.setAttribute("src", "project"+i+".png");
 		projectLink.appendChild(projectImage);
 		project.appendChild(projectLink);
 		projectDiv.appendChild(project);
